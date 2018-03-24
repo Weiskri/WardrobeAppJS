ed3ee41e47823c545666efc4c0b6ae899cf9840f
Weiskri <kkweis13@gmail.com>
2018-03-23 22:45:24 -0400

    one more javascript function

--- WardrobeApp/Content/JavaScript.js
+++ WardrobeApp/Content/JavaScript.js
@@ -34,3 +34,55 @@ function Redirect(x) {
         win.focus();
     }
 }
+
+function trivia() {
+    var triviaArray = ["Cabbage", " romaine", " endive", " avocado."]
+    var correctAnswerArray = ["cabbage", "avocado"]
+    var userResponse = prompt('Of the following foods, which can be fatal to a rabbit?\n' + triviaArray);
+
+    if (correctAnswerArray.includes(userResponse)) {
+        alert('Correct');
+    }
+
+    else {
+        alert('Incorrect');
+    }
+
+}
+
+function triviaBox() {
+    var x = document.getElementById('trivia-ribbon')
+    x.style.display = "none";
+
+}
+
+
+function bounce() {
+
+    var x = document.getElementById("stuff"); // points to h1 by ID
+
+    x.style.WebkitAnimation = "jackInTheBox 4s 2";
+    x.style.animation = "jackInTheBox 4s 2";       // for other browsers
+
+
+    x.addEventListener("webkitAnimationStart", myStartFunction);
+    x.addEventListener("webkitAnimationIteration", myRepeatFunction);
+    x.addEventListener("webkitAnimationEnd", myEndFunction);
+
+    // other browsers
+    x.addEventListener("animationstart", myStartFunction);
+    x.addEventListener("animationiteration", myRepeatFunction);
+    x.addEventListener("animationend", myEndFunction);
+}
+
+function myStartFunction() {
+    this.style.color = "#9b7da3";
+}
+
+function myRepeatFunction() {
+    this.style.color = "#82b1ba";
+}
+
+function myEndFunction() {
+    this.style.color = "#b8b2ba";
+}
